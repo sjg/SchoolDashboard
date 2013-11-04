a775a2c5a01baf47ddcd09e2d6b3c7b1d171c486
Steven Gray <steve@stevenjamesgray.com>
2013-11-04 17:54:56 +0000

    Added Edit widget function
    Work in progress
    
    Signed-off-by: Steven Gray <steve@stevenjamesgray.com>

--- common_assets/add.js
+++ common_assets/add.js
@@ -2,6 +2,8 @@ var ios_feeds_url = "../common_assets/proxyFetch.php?feeds";
 var ios_cat_url = "../common_assets/proxyFetch.php?cat";
 var components = "../common_assets/components.json";
 
+var editToggle = 0;
+
 $(document).ready(function() {
 	$("#addButton").click( function(){
 		$('#addModal').modal('toggle');
@@ -9,6 +11,28 @@ $(document).ready(function() {
 
 	$("#editButton").click( function(){
 		$('#addModal').modal('hide');
+		
+		if(editToggle == 1){
+			$.each($('.gridster li'), function(k,v){
+				$(this).find(".widgetClose").remove();
+				editToggle = 0;
+				gridster.enable();
+			});
+		}else{
+			$.each($('.gridster li'), function(k,v){
+				$(this).append("<div class='widgetClose'>&times;</div>");
+				editToggle = 1;
+				gridster.disable();
+			});
+		};
+	});
+
+	$(document).on("click", ".widgetClose", function(){
+		console.log($(this).parent());
+	});
+
+	$(".widgetClose").live(function(){
+		console.log($(this).parent());
 	});
 
 	//$( ".typeSelectable" ).selectable();
@@ -20,9 +44,8 @@ $(document).ready(function() {
 		$(".dynamicStep3").html("Preview");
 		$.getJSON(components, function(data) {
 			$.each(data.widgetList, function(k, v){
-				$(".step2List").append('<li>' +v.name+'</li>');
+				$(".step2List").append('<li id="' + v.name + '" data-feedID="' + v.name + '" data-type="local">' +v.name+'</li>');
 			});
-			//$( ".typeSelectable" ).selectable();
 		});
 	});
 
@@ -55,7 +78,7 @@ $(document).ready(function() {
 					if(s.rel == "urn:X-xively:rels:hasTitle:en"){
 						var title = s.val;
 						var eleID = v.href.replace("/cat/feeds/", "");
-						$(".step2List").append('<li id="'+ eleID +'" data-feedID="' + eleID + '" data-streamID="">' + title +'</li>');
+						$(".step2List").append('<li id="'+ eleID +'" data-feedID="' + eleID + '" data-streamID="" data-type="remote">' + title +'</li>');
 					}
 				});
 			});
@@ -67,16 +90,35 @@ $(document).ready(function() {
 		selected: function( event, ui ) {
 
 			var feedID = $("#" + ui.selected.id).attr("data-feedID");
-			console.log('Selected ' + feedID);
+			var type = $("#" + ui.selected.id).attr("data-type");
+			
+			console.log('Selected feedID: ' + feedID);
+			console.log("Selected type: " + type);
+ 
 
-			if(xively != undefined){
-				xively.feed.get(feedID, function (datastream) {
+			if(type == "remote"){
+				if(xively != undefined){
+					xively.feed.get(feedID, function (datastream) {
+						$(".step3List").html("");
+						$.each(datastream.datastreams, function(k,v){
+							var eleID = feedID + ":" + streamID;
+							var streamID = v.id;
+							var title = v.unit.label;
+							$(".step3List").append('<li id="'+ eleID +'" data-feedID="' + feedID + '" data-streamID="' + streamID +'">' + title +'</li>');
+						});
+					});
+				}
+			}
+
+			if(type == "local"){
+				$.getJSON(components, function(data) {
 					$(".step3List").html("");
-					$.each(datastream.datastreams, function(k,v){
-						var eleID = feedID + ":" + streamID;
-						var streamID = v.id;
-						var title = v.unit.label;
-						$(".step3List").append('<li id="'+ eleID +'" data-feedID="' + feedID + '" data-streamID="' + streamID +'">' + title +'</li>');
+					$.each(data.widgetList, function(k, v){
+						if(v.name == feedID){
+							$.each(v.widgets, function(j, s){
+								$(".step3List").append('<li id="' + s.name + '" data-feedID="' + s.name + '" data-type="local">' +s.name+'</li>');
+							});
+						}
 					});
 				});
 			}
