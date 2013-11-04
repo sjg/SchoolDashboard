44ef4f377c1cc7d8d79fee8ad60c884ddbc9680e
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2013-11-04 12:31:36 +0000

    Remove extraneous console logging

--- common_assets/app.js
+++ common_assets/app.js
@@ -133,12 +133,10 @@ fb.on('value', function(snapshot){
             }
     }}).data('gridster');
 });
-// 
-            console.log(value.schoolName);
+
 // populate a dropdown with a JSON object containing feed details and the school name
 $(document).ready(function() {
     $.getJSON("../common_assets/schools.json", function(data) {
-        // console.log(data);
         $.each(data.schools, function(key, value) {
             var option = $('<option />').val(value.activeFeeds).text(value.schoolName);
             $("#schools").append(option);
